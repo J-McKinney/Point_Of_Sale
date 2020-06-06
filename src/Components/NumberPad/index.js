81b810d76b6f1bdb9c71a63abfbf11a20a182447
Jesse McKinney <jessemckinney513@gmail.com>
2020-06-06 14:20:43 -0400

    started the number pad

--- src/Components/NumberPad/index.js
+++ src/Components/NumberPad/index.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function NumberPad(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <Container id="numberPadContainer">
+        <Row>
+          <Button id="one" className="numbers">1</Button>
+          <Button id="two" className="numbers">2</Button>
+          <Button id="three" className="numbers">3</Button>
+        </Row>
+        <Row>
+          <Button id="four" className="numbers">4</Button>
+          <Button id="five" className="numbers">5</Button>
+          <Button id="six" className="numbers">6</Button>
+        </Row>
+        <Row>
+          <Button id="seven" className="numbers">7</Button>
+          <Button id="eight" className="numbers">8</Button>
+          <Button id="nine" className="numbers">9</Button>
+        </Row>
+        <Row>
+          <Col>
+            <Button id="zero" className="numbers">0</Button>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default NumberPad;
--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import NumberPad from "../Components/NumberPad";
 
 class LoginPage extends Component {
-    render() {
-        return (
-            <>
-            
-            </>
-        );
-    }
+  render() {
+    return (
+      <>
+        <NumberPad />
+      </>
+    );
+  }
 }
 
 export default LoginPage;
