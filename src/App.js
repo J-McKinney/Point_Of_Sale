794296d095da8ba4439c9bab8ed3bce29d02f128
Jesse McKinney <jessemckinney513@gmail.com>
2020-06-06 12:28:25 -0400

    added footer and wrapper

--- src/App.js
+++ src/App.js
@@ -3,15 +3,22 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import LoginPage from "./Pages/LoginPage";
 import UserPage from "./Pages/UserPage";
 import AdminPage from "./Pages/AdminPage";
+import Wrapper from "./Components/Wrapper";
+import Footer from "./Components/Footer";
 import "./App.css";
 
 function App() {
   return (
     <>
       <Router>
-        <Route exact path="/" component={LoginPage} />
-        <Route exact path="/UserPage" component={UserPage} />
-        <Route exact path="/AdminPage" component={AdminPage} />
+        <div>
+          <Wrapper>
+            <Route exact path="/" component={LoginPage} />
+            <Route exact path="/UserPage" component={UserPage} />
+            <Route exact path="/AdminPage" component={AdminPage} />
+          </Wrapper>
+          <Footer />
+        </div>
       </Router>
     </>
   );
