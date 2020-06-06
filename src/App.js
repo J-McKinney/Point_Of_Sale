b5810aa906d5fc00a0609c29f6a4ac319870677f
Jesse McKinney <jessemckinney513@gmail.com>
2020-06-06 00:09:56 -0400

    yay

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import LoginPage from "./Pages/LoginPage";
+import UserPage from "./Pages/UserPage";
+import AdminPage from "./Pages/AdminPage";
 import "./App.css";
 
 function App() {
   return (
     <>
       <Router>
-        <Route></Route>
-        <Route></Route>
-        <Route></Route>
+        <Route exact path="/" component={LoginPage} />
+        <Route exact path="/UserPage" component={UserPage} />
+        <Route exact path="/AdminPage" component={AdminPage} />
       </Router>
     </>
   );
