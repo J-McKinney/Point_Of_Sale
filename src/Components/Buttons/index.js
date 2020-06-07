318a8943fc6b601734271da1fbe66528e4c54c34
Jesse McKinney <jessemckinney513@gmail.com>
2020-06-06 21:04:09 -0400

    yay

---
+++ src/Components/Buttons/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from "react-bootstrap/Button";
+
+function Buttons(props) {
+    return (
+        <div>
+            <Button></Button>
+        </div>
+    );
+}
+
+export default Buttons;
