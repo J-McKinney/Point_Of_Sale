e03374b64a910cb99e8a855eeef03f9213a554b2
Jesse McKinney <jessemckinney513@gmail.com>
2020-06-07 14:10:30 -0400

    put the keyboard npm in its own package

--- src/Components/KeyBoard/index.js
+++ src/Components/KeyBoard/index.js
@@ -1,100 +1,61 @@
-import React from "react";
-import Button from "react-bootstrap/Button";
+import React, { Component } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 // import Col from "react-bootstrap/Col";
+import Keyboard from "react-simple-keyboard";
+import "./style.css";
 
-function KeyBoard(props) {
-  return (
-    <div>
-      <Container id="keyBoardContainer">
-        <Row>
-          <Button value="Q" id="Q" className="letters">
-            Q
-          </Button>
-          <Button value="W" id="W" className="letters">
-            W
-          </Button>
-          <Button value="E" id="E" className="letters">
-            E
-          </Button>
-          <Button value="R" id="R" className="letters">
-            R
-          </Button>
-          <Button value="T" id="T" className="letters">
-            T
-          </Button>
-          <Button value="Y" id="Y" className="letters">
-            Y
-          </Button>
-          <Button value="U" id="U" className="letters">
-            U
-          </Button>
-          <Button value="I" id="I" className="letters">
-            I
-          </Button>
-          <Button value="O" id="O" className="letters">
-            O
-          </Button>
-          <Button value="P" id="P" className="letters">
-            P
-          </Button>
-        </Row>
-        <Row>
-          <Button value="A" id="A" className="letters">
-            A
-          </Button>
-          <Button value="S" id="S" className="letters">
-            S
-          </Button>
-          <Button value="D" id="D" className="letters">
-            D
-          </Button>
-          <Button value="F" id="F" className="letters">
-            F
-          </Button>
-          <Button value="G" id="G" className="letters">
-            G
-          </Button>
-          <Button value="H" id="H" className="letters">
-            H
-          </Button>
-          <Button value="J" id="J" className="letters">
-            J
-          </Button>
-          <Button value="K" id="K" className="letters">
-            K
-          </Button>
-          <Button value="L" id="L" className="letters">
-            L
-          </Button>
-        </Row>
-        <Row>
-          <Button value="Z" id="Z" className="letters">
-            Z
-          </Button>
-          <Button value="X" id="X" className="letters">
-            X
-          </Button>
-          <Button value="C" id="C" className="letters">
-            C
-          </Button>
-          <Button value="V" id="V" className="letters">
-            V
-          </Button>
-          <Button value="B" id="B" className="letters">
-            B
-          </Button>
-          <Button value="N" id="N" className="letters">
-            N
-          </Button>
-          <Button value="M" id="M" className="letters">
-            M
-          </Button>
-        </Row>
-      </Container>
-    </div>
-  );
+class KeyBoard extends Component {
+  state = {
+    layoutName: "default",
+    input: "",
+  };
+
+  onChange = (input) => {
+    this.setState({ input });
+    console.log("Input changed", input);
+  };
+
+  onKeyPress = (button) => {
+    console.log("Button pressed", button);
+    // If you want to handle the shift and caps lock buttons
+    if (button === "{shift}" || button === "{lock}") this.handleShift();
+  };
+
+  handleShift = () => {
+    const layoutName = this.state.layoutName;
+    this.setState({
+      layoutName: layoutName === "default" ? "shift" : "default",
+    });
+  };
+
+  onChangeInput = (event) => {
+    const input = event.target.value;
+    this.setState({ input });
+    this.keyboard.setInput(input);
+  };
+
+  render() {
+    return (
+      <>
+        <Container id="keyBoardContainer">
+          <Row id="keyBoardRow">
+            <input
+              value={this.state.input}
+              placeholder={"Tap on the virtual keyboard to start"}
+              onChange={this.onChangeInput}
+            />
+            <Keyboard
+              keyboardRef={(r) => (this.keyboard = r)}
+              layoutName={this.state.layoutName}
+              onChange={this.onChange}
+              onKeyPress={this.onKeyPress}
+            />
+          </Row>
+        </Container>
+      </>
+    );
+  }
 }
 
 export default KeyBoard;
--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,55 +1,12 @@
 import React, { Component } from "react";
-import NumberPad from "../Components/NumberPad";
 import KeyBoard from "../Components/KeyBoard";
-import Keyboard from "react-simple-keyboard";
 
 class LoginPage extends Component {
-  state = {
-    layoutName: "default",
-    input: "",
-  };
-
-  onChange = (input) => {
-    this.setState({ input });
-    console.log("Input changed", input);
-  };
-
-  onKeyPress = (button) => {
-    console.log("Button pressed", button);
-    // If you want to handle the shift and caps lock buttons
-    if (button === "{shift}" || button === "{lock}") this.handleShift();
-  };
-
-  handleShift = () => {
-    const layoutName = this.state.layoutName;
-    this.setState({
-      layoutName: layoutName === "default" ? "shift" : "default",
-    });
-  };
-
-  onChangeInput = (event) => {
-    const input = event.target.value;
-    this.setState({ input });
-    this.keyboard.setInput(input);
-  };
 
   render() {
     return (
       <>
-        <NumberPad />
-        <KeyBoard />
-        <br />
-        <input
-          value={this.state.input}
-          placeholder={"Tap on the virtual keyboard to start"}
-          onChange={this.onChangeInput}
-        />
-        <Keyboard
-          keyboardRef={(r) => (this.keyboard = r)}
-          layoutName={this.state.layoutName}
-          onChange={this.onChange}
-          onKeyPress={this.onKeyPress}
-        />
+      <KeyBoard />
       </>
     );
   }
