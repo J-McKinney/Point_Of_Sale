af3af5b58d6b927588952263dd0a16c04a9a7391
Jesse McKinney <jessemckinney513@gmail.com>
2020-06-09 17:47:32 -0400

    setting state of loginID to the input, clearing it and using the built in getInput method

--- src/Components/KeyBoard/index.js
+++ src/Components/KeyBoard/index.js
@@ -41,6 +41,10 @@ class KeyBoard extends Component {
   };
 
   loginCreds = () => {
+    // Using the getInput Method to get the input
+    let input = this.keyboard.getInput()
+    console.log("LoginCreds: " + input)
+    // setting the state of LoginID to the input for credential authorization
     this.setState({ loginID: this.state.input });
     this.keyboard.clearInput();
   };
