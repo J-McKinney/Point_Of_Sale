f21a35fd7bdda1dd882b4634b7b7013e2476327e
Jesse McKinney <jessemckinney513@gmail.com>
2020-06-10 14:43:00 -0400

    yay

--- src/Components/KeyBoard/index.js
+++ src/Components/KeyBoard/index.js
@@ -11,7 +11,7 @@ class KeyBoard extends Component {
   };
 
   componentDidUpdate() {
-    console.log("componentDidUpdate: " + this.state.loginID);
+    // console.log("componentDidUpdate: " + this.state.loginID);
   }
 
   onChange = (input) => {
@@ -20,7 +20,7 @@ class KeyBoard extends Component {
   };
 
   onKeyPress = (button) => {
-    console.log("Button pressed: ", button);
+    // console.log("Button pressed: ", button);
     // If you want to handle the shift and caps lock buttons
     if (button === "{shift}" || button === "{lock}") this.handleShift();
     // need to get the {enter} button to log in users with authentication
@@ -42,8 +42,8 @@ class KeyBoard extends Component {
 
   loginCreds = () => {
     // Using the getInput Method to get the input
-    let input = this.keyboard.getInput()
-    console.log("LoginCreds: " + input)
+    let input = this.keyboard.getInput();
+    console.log("LoginCreds: " + input);
     // setting the state of LoginID to the input for credential authorization
     this.setState({ loginID: this.state.input });
     this.keyboard.clearInput();
@@ -52,18 +52,20 @@ class KeyBoard extends Component {
   render() {
     return (
       <>
-        <input
-          value={this.state.input}
-          placeholder={"Login Identification"}
-          onChange={this.onChangeInput}
-        />
-        <Keyboard
-          keyboardRef={(r) => (this.keyboard = r)}
-          theme={"hg-theme-default myTheme1"} // This is the dark theme mode
-          layoutName={this.state.layoutName}
-          onChange={this.onChange}
-          onKeyPress={this.onKeyPress}
-        />
+        <div id="keyboardWrapper">
+          <input
+            value={this.state.input}
+            placeholder={"Login Identification"}
+            onChange={this.onChangeInput}
+          />
+          <Keyboard
+            keyboardRef={(r) => (this.keyboard = r)}
+            theme={"hg-theme-default myTheme1"} // This is the dark theme mode
+            layoutName={this.state.layoutName}
+            onChange={this.onChange}
+            onKeyPress={this.onKeyPress}
+          />
+        </div>
       </>
     );
   }
